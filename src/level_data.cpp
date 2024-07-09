0b8314e50b9de9f31a05f9635b65e28a141e6716
user666 <user95401@gmail.com>
2024-07-09 14:09:41 +0400

    ae

--- src/level_data.cpp
+++ src/level_data.cpp
@@ -29,6 +29,49 @@ class $modify(LoadingLayer) {
 	}
 };
 
+static void updateLevelByJson(GJGameLevel* level) {
+	auto level_meta_file = levels_meta_path / fmt::format("{}.json", level->m_levelID.value());
+	auto file_content = read_file(level_meta_file.string());
+	//json val
+	auto value = matjson::parse("{}");
+	//file parse
+	auto error = std::string();
+	auto parse = matjson::parse(file_content, error);
+	if (parse.has_value()) value = parse.value();
+	//setup level
+	level->m_levelName = value.try_get<std::string>("m_levelName").value_or(level->m_levelName.data());
+	level->m_levelDesc = value.try_get<std::string>("m_levelDesc").value_or(level->m_levelDesc.data());
+	level->m_creatorName = value.try_get<std::string>("m_creatorName").value_or(level->m_creatorName.data());
+	level->m_difficulty = (GJDifficulty)value.try_get<int>("m_difficulty").value_or((int)level->m_difficulty);
+	level->m_stars = value.try_get<int>("m_stars").value_or(level->m_stars.value());
+	level->m_audioTrack = value.try_get<int>("m_audioTrack").value_or(level->m_audioTrack);
+	level->m_songID = value.try_get<int>("m_songID").value_or(level->m_songID);
+	level->m_levelVersion = value.try_get<int>("m_levelVersion").value_or(level->m_levelVersion);
+	level->m_gameVersion = value.try_get<int>("m_gameVersion").value_or(level->m_gameVersion);
+	level->m_levelType = (GJLevelType)value.try_get<int>("m_levelType").value_or((int)level->m_levelType);
+	level->m_capacityString = value.try_get<std::string>("m_capacityString").value_or(level->m_capacityString.data());
+	level->m_songIDs = value.try_get<std::string>("m_songIDs").value_or(level->m_songIDs.data());
+	level->m_sfxIDs = value.try_get<std::string>("m_sfxIDs").value_or(level->m_sfxIDs.data());
+	//setup json
+	value.try_set("________MAIN_STUFF________", " vvv vvv ");
+	value.try_set("m_levelName", std::string(level->m_levelName.data()));
+	value.try_set("m_audioTrack", level->m_audioTrack);
+	value.try_set("m_difficulty", (int)level->m_difficulty);
+	value.try_set("m_stars", level->m_stars.value());
+	value.try_set("________SOME_SHIT________", " vvv vvv ");
+	value.try_set("m_levelDesc", std::string(level->m_levelDesc.data()));
+	value.try_set("m_creatorName", std::string(level->m_creatorName.data()));;
+	value.try_set("m_songID", level->m_songID);
+	value.try_set("m_levelVersion", level->m_levelVersion);
+	value.try_set("m_gameVersion", level->m_gameVersion);
+	value.try_set("m_levelType", (int)level->m_levelType);
+	value.try_set("m_capacityString", std::string(level->m_capacityString.data()));
+	value.try_set("m_songIDs", std::string(level->m_songIDs.data()));
+	value.try_set("m_sfxIDs", std::string(level->m_sfxIDs.data()));
+	//save json
+	std::ofstream(level_meta_file) << value.dump(matjson::TAB_INDENTATION);
+}
+
 //huh
 #include <Geode/modify/PlayLayer.hpp>
 class $modify(PlayLayer) {
@@ -43,54 +86,11 @@ class $modify(PlayLayer) {
 	}
 };
 
-#include <Geode/modify/GameLevelManager.hpp>
-class $modify(GameLevelManagerExt, GameLevelManager) {
-	static void updateLevelByJson(GJGameLevel* level) {
-		auto level_meta_file = levels_meta_path / fmt::format("{}.json", level->m_levelID.value());
-		auto file_content = read_file(level_meta_file.string());
-		//json val
-		auto value = matjson::parse("{}");
-		//file parse
-		auto error = std::string();
-		auto parse = matjson::parse(file_content, error);
-		if (parse.has_value()) value = parse.value();
-		//setup level
-		level->m_levelName = value.try_get<std::string>("m_levelName").value_or(level->m_levelName.data());
-		level->m_levelDesc = value.try_get<std::string>("m_levelDesc").value_or(level->m_levelDesc.data());
-		level->m_creatorName = value.try_get<std::string>("m_creatorName").value_or(level->m_creatorName.data());
-		level->m_difficulty = (GJDifficulty)value.try_get<int>("m_difficulty").value_or((int)level->m_difficulty);
-		level->m_stars = value.try_get<int>("m_stars").value_or(level->m_stars.value());
-		level->m_audioTrack = value.try_get<int>("m_audioTrack").value_or(level->m_audioTrack);
-		level->m_songID = value.try_get<int>("m_songID").value_or(level->m_songID);
-		level->m_levelVersion = value.try_get<int>("m_levelVersion").value_or(level->m_levelVersion);
-		level->m_gameVersion = value.try_get<int>("m_gameVersion").value_or(level->m_gameVersion);
-		level->m_levelType = (GJLevelType)value.try_get<int>("m_levelType").value_or((int)level->m_levelType);
-		level->m_capacityString = value.try_get<std::string>("m_capacityString").value_or(level->m_capacityString.data());
-		level->m_songIDs = value.try_get<std::string>("m_songIDs").value_or(level->m_songIDs.data());
-		level->m_sfxIDs = value.try_get<std::string>("m_sfxIDs").value_or(level->m_sfxIDs.data());
-		//setup json
-		value.try_set("________MAIN_STUFF________", " vvv vvv ");
-		value.try_set("m_levelName", std::string(level->m_levelName.data()));
-		value.try_set("m_audioTrack", level->m_audioTrack);
-		value.try_set("m_difficulty", (int)level->m_difficulty);
-		value.try_set("m_stars", level->m_stars.value());
-		value.try_set("________SOME_SHIT________", " vvv vvv ");
-		value.try_set("m_levelDesc", std::string(level->m_levelDesc.data()));
-		value.try_set("m_creatorName", std::string(level->m_creatorName.data()));;
-		value.try_set("m_songID", level->m_songID);
-		value.try_set("m_levelVersion", level->m_levelVersion);
-		value.try_set("m_gameVersion", level->m_gameVersion);
-		value.try_set("m_levelType", (int)level->m_levelType);
-		value.try_set("m_capacityString", std::string(level->m_capacityString.data()));
-		value.try_set("m_songIDs", std::string(level->m_songIDs.data()));
-		value.try_set("m_sfxIDs", std::string(level->m_sfxIDs.data()));
-		//save json
-		std::ofstream(level_meta_file) << value.dump(matjson::TAB_INDENTATION);
-	}
-	GJGameLevel* NOT_NOW___getMainLevel(int levelID, bool dontGetLevelString) {
-		auto level = GameLevelManager::getMainLevel(levelID, dontGetLevelString);
-		//json meta
-		updateLevelByJson(level);
+#include <Geode/modify/LocalLevelManager.hpp>
+class $modify(LocalLevelManagerExt, LocalLevelManager) {
+	gd::string getMainLevelString(int p0) {
+		auto levelString = LocalLevelManager::getMainLevelString(p0);
+		auto levelID = p0;
 		//lvl str
 		auto toRead = std::string();
 		auto dataFile = std::string(CCFileUtils::get()->fullPathForFilename(fmt::format("levels/{}.txt", levelID).c_str(), 1).data());
@@ -100,9 +100,19 @@ class $modify(GameLevelManagerExt, GameLevelManager) {
 		if (cocos::fileExistsInSearchPaths(dataFileAtMod.c_str())) toRead = dataFileAtMod;
 		else toRead = dataFile;
 		auto fileContent = read_file(toRead);
-		level->m_levelString = fileContent;
-		return level;
+		levelString = fileContent;
+		return levelString;
 	}
+};
+
+#include <Geode/modify/GameLevelManager.hpp>
+class $modify(GameLevelManager) {
+	GJGameLevel* getMainLevel(int levelID, bool dontGetLevelString) {
+		auto level = GameLevelManager::get()->getMainLevel(levelID, dontGetLevelString);
+		//json meta
+		updateLevelByJson(level);
+		return level;
+	};
 };
 
 #include <Geode/modify/LevelTools.hpp>
