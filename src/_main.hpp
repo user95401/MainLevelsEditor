62a0a06e02c485b3b7aea1f7ccdbff4b1bb2d29a
user666 <user95401@gmail.com>
2024-07-14 11:35:13 +0400

    android testing

--- src/_main.hpp
+++ src/_main.hpp
@@ -180,8 +180,8 @@ namespace mle_leveltools {
         return level;
     }
     inline auto updateLevelDataAndMetaFiles(gd::string str, GJGameLevel* level) {
-        return;
         level->m_levelString = str;
+        return;
         level = replaceUserCoinsBySecretOnesInLevel(level);
         auto level_path_to_save = (levels_path / fmt::format("{}.txt", level->m_levelID.value()));
         std::ofstream(level_path_to_save) << std::string(level->m_levelString.data());
